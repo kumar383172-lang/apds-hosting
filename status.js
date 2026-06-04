367cd7301f09859eb1d9cf67620f7fe22af90bd9
ADITYA KUMAR THAKUR <kumar383172@gmail.com>
2026-06-04 19:30:29 +0530

    Add files via upload

---
+++ status.js
@@ -0,0 +1,6 @@
+let button = document.getElementById("checkBtn");
+let status = document.getElementById("statusBox");
+
+button.addEventListener("click",function(){
+    status.style.display="block";
+});
